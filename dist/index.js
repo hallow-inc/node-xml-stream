4c34eba2c5eb867230880a42fcf72fab4e7a4f98
VersorVerbi <doroapodeus@gmail.com>
2023-07-12 15:59:44 -0500

    Add meager typing

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var __importDefault = undefined && undefined.__importDefault || function (mod) {
+    return mod && mod.__esModule ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var parser_1 = __importDefault(require("./parser"));
+module.exports = parser_1.default;
+exports.default = parser_1.default;
